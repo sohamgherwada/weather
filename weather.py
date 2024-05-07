5e5b721e189abb59e8ddf0a1dc857e1f7cf23d8e
sohamgherwada <112994330+sohamgherwada@users.noreply.github.com>
2024-05-07 17:39:45 -0400

    This is my API implementation of the weather

---
+++ weather.py
@@ -0,0 +1,19 @@
+import requests
+import sys
+
+def main():
+    while True:
+        city = input("Enter the city (or 'end' to exit): ")
+        if city == "end":
+            break
+        
+        city = city.replace(" ", "%20")
+        api_key = "9151390c137bd7d44f059851314e63ca"
+        base_url = f"http://api.weatherstack.com/current?access_key={api_key}&query={city}"
+        
+        response = requests.get(base_url)
+        data = response.json()
+        
+        print(data['current']['temperature'])
+
+main()
